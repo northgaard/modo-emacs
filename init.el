ba573238409c1667454f50c595004f800eb0b6f3
Sebastian Nørgaard <sebnorg@mek.dtu.dk>
2017-11-09 13:59:05 +0100

    Added use-package.

--- init.el
+++ init.el
@@ -11,3 +11,22 @@
 ;; Bootstrap using local install
 (setq quelpa-ci-dir (concat user-emacs-directory "packages/quelpa"))
 (load (expand-file-name "bootstrap.el" quelpa-ci-dir))
+
+;;; use-package
+(quelpa `(diminish :fetcher file
+		   :path ,(expand-file-name "diminish.el"
+					    (concat user-emacs-directory
+						    "packages/diminish"))
+		   :version original))
+(quelpa `(bind-key :fetcher file
+		   :path ,(expand-file-name "bind-key.el"
+					    (concat user-emacs-directory
+						    "packages/use-package"))
+		   :version original))
+(quelpa `(use-package :fetcher file
+		      :path ,(expand-file-name "use-package.el"
+					       (concat user-emacs-directory
+						       "packages/use-package"))))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
