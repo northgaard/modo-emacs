dd857afe34d65f5ef148334aabff3f127917a401
Sebastian Nørgaard <sebnorg@mek.dtu.dk>
2017-11-09 19:15:53 +0100

    Added macros for building packages with quelpa. Added global directory variables.

--- init.el
+++ init.el
@@ -1,7 +1,13 @@
+(defvar modo-emacs-dir (expand-file-name user-emacs-directory)
+  "The path to the emacs.d directory containing the modo emacs files.")
+
+(defvar modo-repo-dir (concat modo-emacs-dir "repos/")
+  "The directory containing the modo repositories.")
+
 ;;; package.el
 (setq package-enable-at-startup nil)
 (setq package-archives nil)
-(setq package-user-dir (expand-file-name "build" user-emacs-directory))
+(setq package-user-dir (concat modo-emacs-dir "build/"))
 ;; (package-initialize) is called when quelpa is bootstrapped
 
 ;;; Initialize quelpa
@@ -10,44 +16,34 @@
 (setq quelpa-update-melpa-p nil)
 (setq quelpa-checkout-melpa-p nil)
 ;; Bootstrap using local install
-(setq quelpa-ci-dir (concat user-emacs-directory "packages/quelpa"))
+(setq quelpa-ci-dir (concat modo-repo-dir "quelpa"))
 (load (expand-file-name "bootstrap.el" quelpa-ci-dir))
 
+(defmacro modo-add-package (pkg dir)
+  "Builds the package PKG from the directory DIR found in modo-repo-dir."
+  `(quelpa (quote (,pkg :fetcher file
+			:path ,(concat modo-repo-dir dir)))))
+
+(defmacro modo-add-package-single (pkg file)
+  "Builds the single-file package PKG from the file FILE found in modo-repo-dir."
+  `(quelpa (quote (,pkg :fetcher file
+			:path ,(expand-file-name file modo-repo-dir)
+			:version original))))
+
 ;;; use-package
-(quelpa `(diminish :fetcher file
-		   :path ,(expand-file-name "diminish.el"
-					    (concat user-emacs-directory
-						    "packages/diminish"))
-		   :version original))
-(quelpa `(bind-key :fetcher file
-		   :path ,(expand-file-name "bind-key.el"
-					    (concat user-emacs-directory
-						    "packages/use-package"))
-		   :version original))
-(quelpa `(use-package :fetcher file
-		      :path ,(expand-file-name "use-package.el"
-					       (concat user-emacs-directory
-						       "packages/use-package"))
-		      :version original))
-(eval-when-compile
-  (setq use-package-verbose t)
-  (setq use-package-always-defer t)
-  (require 'use-package))
+(modo-add-package-single diminish "diminish/diminish.el")
+(modo-add-package-single bind-key "use-package/bind-key.el")
+(modo-add-package-single use-package "use-package/use-package.el")
+
+(setq use-package-verbose t)
+(setq use-package-always-defer t)
+(require 'use-package)
 (require 'diminish)
 
-;;; evil-mode
-(quelpa `(undo-tree :fetcher file
-		    :path ,(expand-file-name "undo-tree.el"
-					     (concat user-emacs-directory
-						     "packages/evil/lib"))
-		    :version original))
-(quelpa `(goto-chg :fetcher file
-		   :path ,(expand-file-name "goto-chg.el"
-					    (concat user-emacs-directory
-						    "packages/evil/lib"))
-		   :version original))
-(quelpa `(evil :fetcher file
-	       :path ,(concat user-emacs-directory "packages/evil")))
+;;; evil mode
+(modo-add-package-single undo-tree "evil/lib/undo-tree.el")
+(modo-add-package-single goto-chg "evil/lib/goto-chg.el")
+(modo-add-package evil "evil")
 (use-package evil :demand t
   :config
   (evil-mode 1))
