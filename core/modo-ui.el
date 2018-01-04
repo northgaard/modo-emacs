fb30205c936c1b5d7a1ce2be538e91846fff39c6
Sebastian Nørgaard <sebnorg@mek.dtu.dk>
2018-01-04 16:44:08 +0100

    Start in fundamental mode.

--- core/modo-ui.el
+++ core/modo-ui.el
@@ -6,9 +6,6 @@
 ;;; Code:
 
 ;;; Base settings
-(setq inhibit-splash-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (when (boundp 'scroll-bar-mode)
