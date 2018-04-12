bfac5afdf548f87b79a929f0f519e53917cc015b
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2018-04-12 14:14:03 +0200

    Add keybinds for C#.
    Proper leader keybinds are not in place; see previous commit for details.

--- modules/modo-csharp.el
+++ modules/modo-csharp.el
@@ -5,27 +5,47 @@
 
 ;;; Code:
 
+(defun modo-omnisharp-setup-hook ()
+  (require 'company)
+  (add-to-list 'company-backends #'company-omnisharp)
+  (setq-local company-idle-delay 0.3)
+  (setq-local company-minimum-prefix-length 3)
+  (add-hook 'before-save-hook #'omnisharp-code-format-entire-file nil t))
+
+(defun omnisharp-add-dot-and-company-complete ()
+  (interactive)
+  (insert ".")
+  (company-complete))
+
 (straight-use-package 'csharp-mode)
-(use-package csharp-mode)
+(use-package csharp-mode
+  :hook ((csharp-mode . flycheck-mode)
+         (csharp-mode . omnisharp-mode))
+  :config
+  (require 'omnisharp)
+  (general-define-key :states 'normal
+                      :keymaps 'csharp-mode-map
+                      "gd" 'omnisharp-go-to-definition)
+  (general-define-key :states '(motion normal visual)
+                      :keymaps 'csharp-mode-map
+                      ",u" 'omnisharp-find-usages
+                      ",p" 'omnisharp-find-implementations
+                      ",i" 'omnisharp-current-type-information
+                      ",I" 'omnisharp-current-type-documentation
+                      ",n" 'omnisharp-rename
+                      ",r" 'omnisharp-run-code-action-refactoring
+                      ",c" 'recompile)
+  (general-define-key :states 'insert
+                      :keymaps 'csharp-mode-map
+                      "." 'omnisharp-add-dot-and-company-complete))
 
 (straight-use-package 'omnisharp)
 (use-package omnisharp
-  :demand t
   :diminish omnisharp-mode
-  :after csharp-mode
-  :hook ((csharp-mode . flycheck-mode)
-         (csharp-mode . omnisharp-mode)
-         (omnisharp-mode . modo-omnisharp-setup-hook))
+  :hook ((omnisharp-mode . modo-omnisharp-setup-hook))
   :init
   (setq omnisharp-autocomplete-want-documentation nil
-        omnisharp-cache-directory (concat modo-cache-dir "omnisharp"))
-  :config
-  (defun modo-omnisharp-setup-hook ()
-    (require 'company)
-    (add-to-list 'company-backends #'company-omnisharp)
-    (setq-local company-idle-delay 0.3)
-    (setq-local company-minimum-prefix-length 3)
-    (add-hook 'before-save-hook #'omnisharp-code-format-entire-file nil t)))
+        omnisharp-cache-directory (concat modo-cache-dir "omnisharp")))
 
 (provide 'modo-csharp)
 ;;; modo-csharp.el ends here
