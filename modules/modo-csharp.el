742be3e58b94d2f63b7f8dd74c61f712da2d4722
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2018-04-12 12:55:59 +0200

    Modify omnisharp setup.

--- modules/modo-csharp.el
+++ modules/modo-csharp.el
@@ -11,16 +11,21 @@
 (straight-use-package 'omnisharp)
 (use-package omnisharp
   :demand t
+  :diminish omnisharp-mode
   :after csharp-mode
   :hook ((csharp-mode . flycheck-mode)
-         (csharp-mode . omnisharp-mode))
+         (csharp-mode . omnisharp-mode)
+         (omnisharp-mode . modo-omnisharp-setup-hook))
   :init
   (setq omnisharp-autocomplete-want-documentation nil
         omnisharp-cache-directory (concat modo-cache-dir "omnisharp"))
   :config
-  (with-eval-after-load 'company
-    (add-to-list 'company-backends #'company-omnisharp)))
-                           
+  (defun modo-omnisharp-setup-hook ()
+    (require 'company)
+    (add-to-list 'company-backends #'company-omnisharp)
+    (setq-local company-idle-delay 0.3)
+    (setq-local company-minimum-prefix-length 3)
+    (add-hook 'before-save-hook #'omnisharp-code-format-entire-file nil t)))
 
 (provide 'modo-csharp)
 ;;; modo-csharp.el ends here
