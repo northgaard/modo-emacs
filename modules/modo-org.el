b7dd2f6d700baa726c79e23f613372b5e20d1fb1
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2018-08-29 16:43:36 +0200

    Minor refactor and addition to org module

--- modules/modo-org.el
+++ modules/modo-org.el
@@ -9,6 +9,10 @@
 (straight-use-package 'org-plus-contrib)
 (use-package org
   :config
+  (defun modo-org-mode-setup-hook ()
+    (org-bullets-mode 1)
+    (evil-org-mode 1)
+    (evil-normalize-keymaps))
   ;; Make double extra sure that the built-in org-version is not loaded
   (when (featurep 'org-version)
     (unload-feature 'org-version t))
@@ -17,8 +21,7 @@
   (setq org-startup-indented t)
   (setq org-src-tab-acts-natively t)
   (setq org-src-fontify-natively t)
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  (add-hook 'org-mode-hook (lambda () (evil-org-mode 1)))
+  (add-hook 'org-mode-hook #'modo-org-mode-setup-hook)
   (use-package org-drill :demand t
     :init
     (require 'cl) ;; Needs the outdated cl lib
@@ -38,7 +41,13 @@
   :after org
   :commands (evil-org-mode)
   :config
-  (evil-org-set-key-theme '(navigation insert textobjects additional)))
+  (evil-org-set-key-theme '(navigation insert textobjects additional))
+  (evilem-make-motion modo-avy-org-goto-header #'org-previous-visible-heading
+                      :initial-point 'point-max)
+  (general-define-key :states '(motion normal visual)
+                      :keymaps 'evil-org-mode-map
+                      "gs" #'modo-avy-org-goto-header
+                      "gS" #'avy-org-goto-heading-timer))
 
 (provide 'modo-org)
 ;;; modo-org.el ends here
