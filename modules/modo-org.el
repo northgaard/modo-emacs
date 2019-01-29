cd469d37c213b8fbbb86e71353399f7044d6b90e
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2019-01-29 17:00:10 +0100

    Refactor and fixes for org module

--- modules/modo-org.el
+++ modules/modo-org.el
@@ -5,22 +5,25 @@
 
 ;;; Code:
 
+(defun modo-org-mode-setup ()
+  (org-bullets-mode 1)
+  (evil-org-mode 1)
+  (evil-normalize-keymaps))
+
 (straight-use-package 'git)
 (straight-use-package 'org-plus-contrib)
 (use-package org
-  :config
-  (defun modo-org-mode-setup-hook ()
-    (org-bullets-mode 1)
-    (evil-org-mode 1))
+  :hook (org-mode . modo-org-mode-setup)
+  :init
   ;; Make double extra sure that the built-in org-version is not loaded
   (when (featurep 'org-version)
     (unload-feature 'org-version t))
   ;; Now load the fix
   (require 'org-version (concat modo-modules-dir "org-version-fix"))
+  :config
   (setq org-startup-indented t)
   (setq org-src-tab-acts-natively t)
   (setq org-src-fontify-natively t)
-  (add-hook 'org-mode-hook #'modo-org-mode-setup-hook)
   (use-package org-drill
     :demand t
     :init
@@ -31,22 +34,20 @@
 
 (straight-use-package 'org-bullets)
 (use-package org-bullets
-  :after org
   :commands (org-bullets-mode)
   :config
   (setq org-bullets-bullet-list '("•")))
 
 (straight-use-package 'evil-org)
 (use-package evil-org
-  :after org
   :commands (evil-org-mode)
+  :custom (evil-org-key-theme '(navigation insert textobjects additional))
   :config
-  (evil-org-set-key-theme '(navigation insert textobjects additional))
+  (evil-org-set-key-theme)
   (evilem-make-motion modo-avy-org-goto-header #'org-previous-visible-heading
                       :initial-point 'point-max)
   (general-define-key :states '(motion normal visual)
                       :keymaps 'evil-org-mode-map
-                      :definer 'minor-mode
                       "gs" #'modo-avy-org-goto-header
                       "gS" #'avy-org-goto-heading-timer))
 
