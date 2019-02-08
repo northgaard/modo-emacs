cf4ae944b0717f390242f221d34fda6937a4e565
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2019-02-08 17:59:21 +0100

    Add the flyspell-lazy package

--- modules/modo-spellcheck.el
+++ modules/modo-spellcheck.el
@@ -23,5 +23,38 @@
     (interactive)
     (flyspell-delete-all-overlays)))
 
+(straight-use-package 'flyspell-lazy)
+(use-package flyspell-lazy
+  :config
+  (setq flyspell-lazy-idle-seconds 1.5
+        flyspell-lazy-window-idle-seconds 15))
+
+(define-minor-mode flyspell-lazy-local-mode
+  "Turn on flyspell-lazy-local-mode.
+
+This is more or less a local equivalent of the global
+`flyspell-lazy-mode', but is local and automatically handles
+turning on `flyspell-mode' as well. Using a local minor mode
+allows for greater granularity in controlling spell checking. The
+base `flyspell-mode' can make typing sluggish for larger buffers,
+and so you would use this mode to prevent it. However, for some
+buffers (for instance magit commit buffers), it's advantageous to
+get the instant feedback of the base `flyspell-mode', and the
+decrease in performance is worth it. Using the flyspell-lazy
+package like this is not possible with the global
+`flyspell-lazy-mode'."
+  :group 'flyspell-lazy
+  (progn
+    (require 'flyspell-lazy)
+    (cond
+     (flyspell-lazy-local-mode
+      (if (derived-mode-p 'prog-mode)
+          (flyspell-prog-mode)
+        (flyspell-mode 1))
+      (flyspell-lazy-load))
+     (t
+      (flyspell-lazy-unload)
+      (flyspell-mode -1)))))
+
 (provide 'modo-spellcheck)
 ;;; modo-spellcheck.el ends here
