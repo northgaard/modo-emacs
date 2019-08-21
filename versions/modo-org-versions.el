82efdba5482b53bd7cfcaf4017647ff4b1357809
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2019-08-21 13:21:26 +0200

    Add org-super-agenda to lockfile

--- versions/modo-org-versions.el
+++ versions/modo-org-versions.el
@@ -4,5 +4,6 @@
  ("f.el" . "8191672377816a1975414cc1f116fd3b94b30bd0")
  ("org" . "9a543b97a54bddcf5fda624389425d2dec9cf2f7")
  ("org-bullets" . "b56f2e3812626f2c4ac1686073d102c71f4a8513")
+ ("org-super-agenda" . "4ad333643276ba1a604e8bec4a72ab68bafb8a94")
  ("s.el" . "03410e6a7a2b11e47e1fea3b7d9899c7df26435e"))
 :neptune
