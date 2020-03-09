5e61383a7940bfd87a7a35de1ed629ae322d7670
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2020-03-09 16:36:25 +0100

    Add custom window dispatch function for use with treemacs

--- modules/modo-treemacs.el
+++ modules/modo-treemacs.el
@@ -18,7 +18,23 @@
          "treemacs-persist-at-last-error" modo-cache-dir))
   :config
   (when (featurep 'doom-themes)
-    (doom-themes-treemacs-config)))
+    (doom-themes-treemacs-config))
+  (defun modo-select-window-0-dispatch ()
+    "Dispatches to a window in the following ordered priority:
+
+1. Active minibuffer
+2. Visible treemacs window
+3. Otherwise, calls `winum-select-window-0-or-10'"
+    (interactive)
+    (pcase (if-let ((minibuff-window (active-minibuffer-window)))
+               minibuff-window
+             (treemacs-current-visibility))
+      ((and (pred windowp) minibuff)
+       (select-window minibuff))
+      ('visible (treemacs-select-window))
+      (_ (winum-select-window-0-or-10))))
+  (modo-define-leader-key :keymaps 'override
+    "0" '(modo-select-window-0-dispatch :wk "select-window-0-dispatch")))
 
 (straight-use-package 'treemacs-evil)
 (use-package treemacs-evil
