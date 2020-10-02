91aefff9bc941d74e0355c4a5bcfd7be8fa5f8e3
Sebastian Nørgaard <sebnorgaard@gmail.com>
2020-10-02 15:24:02 +0200

    Update git packages

--- versions/modo-git-versions.el
+++ versions/modo-git-versions.el
@@ -1,9 +1,9 @@
 (("dash.el" . "721436b04da4e2795387cb48a98ac6de37ece0fd")
  ("emacs-async" . "86aef2c38e7d35e8509b7feeee3e989d825eba91")
  ("evil" . "296932406a0b55474fe4b6cb8db8b7d5e05633aa")
- ("evil-magit" . "0b79aa33a478770865716dc0e09f95d91ec042a2")
+ ("evil-magit" . "88dc26ce59dbf4acb4e2891c79c4bd329553ba56")
  ("git-modes" . "55468314a5f6b77d2c96be62c7005ac94545e217")
- ("magit" . "e293416cee73dc47a6dc7f0bc2f4624d69625634")
- ("transient" . "a269614c69ad8b2703e6e5093d0017d6afad6cca")
- ("with-editor" . "4fe66d4d55c8eacebda53a13cc38e01d32bdaaa2"))
+ ("magit" . "6c1c92e5a2abbdf1c852f7da50e69f0b898e7a72")
+ ("transient" . "96cec8e16e081491bd7e04880e0037bc190fae16")
+ ("with-editor" . "c4768f51c7415119519b4626d8643d60e584098c"))
 :alpha
