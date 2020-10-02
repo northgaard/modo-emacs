1bd202f99e22d760c10a5211a844b48478b05035
Sebastian Nørgaard <sebnorgaard@gmail.com>
2020-10-02 20:19:01 +0200

    Update C++ packages

--- versions/modo-cpp-versions.el
+++ versions/modo-cpp-versions.el
@@ -1,4 +1,4 @@
-(("clang-format" . "2d6a4526a2518b7c0059a8a0dfee156e90a49369")
+(("clang-format" . "e48ff8ae18dc7ab6118c1f6752deb48cb1fc83ac")
  ("cmake" . "9692931f8376c47ea335c90f3544e281ddc4ca8d")
- ("modern-cpp-font-lock" . "02f104701bc34c146d22e3143ae59ef362999098"))
+ ("modern-cpp-font-lock" . "865955d0035382a17a7f03add0d00d0bd812b103"))
 :alpha
