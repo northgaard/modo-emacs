db4a4fd3a815ef4280a93e77e4208892c5d6ee1f
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2021-03-08 11:48:48 +0100

    Fix CI

--- test-startup.sh
+++ test-startup.sh
@@ -2,11 +2,11 @@
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
-                              (url-show-status nil)
-                              (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
-                              (load-path (delq default-directory load-path))
-                              (straight-vc-git-default-clone-depth 1))
+                              (load-path (delq default-directory load-path)))
+                           (custom-set-variables (quote (url-show-status nil))
+                                                 (quote (user-emacs-directory default-directory))
+                                                 (quote (straight-vc-git-default-clone-depth 1)))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook)))'
 echo "Startup successful"
