1153961d57a0b897c0960239b8b669d4aa622f0a
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2021-04-28 10:19:28 +0200

    Reduce x-selection-timeout

--- core/modo-os.el
+++ core/modo-os.el
@@ -52,7 +52,12 @@
 ;;; Linux
 (when IS-LINUX
   (setq select-enable-clipboard t
-        select-enable-primary t)
+        select-enable-primary t
+        ;; Maybe help out with "Timed out waiting for reply from
+        ;; selection owner" issue.
+        ;; See Emacs bug #16737
+        ;; https://lists.gnu.org/archive/html/bug-gnu-emacs/2015-01/msg00529.html
+        x-selection-timeout 50)
 
   (defun modo-insert-primary ()
     "Insert the primary selection at point."
