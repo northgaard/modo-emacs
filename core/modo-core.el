1e5f70b6c8ced007bf7e9e666c211cee03bad3bb
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2021-05-19 16:00:35 +0200

    Update native-comp variable names

--- core/modo-core.el
+++ core/modo-core.el
@@ -128,9 +128,9 @@
 (add-hook 'hack-local-variables-hook 'modo-run-local-vars-mode-hook)
 
 ;; Native comp
-(setq comp-async-report-warnings-errors nil)
-(when (boundp 'comp-eln-load-path)
-  (add-to-list 'comp-eln-load-path (concat modo-cache-dir "eln/")))
+(setq native-comp-async-report-warnings-errors nil)
+(when (boundp 'native-comp-eln-load-path)
+  (add-to-list 'native-comp-eln-load-path (concat modo-cache-dir "eln/")))
 
 ;; Add core dir and modules dir to load path
 (add-to-list 'load-path modo-core-dir)
