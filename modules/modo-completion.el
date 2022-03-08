98f474edcc9a1485c329a6f3736b956b9c8ce941
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2022-03-08 11:14:52 +0100

    Simplify previous change
    which-func-update already checks for which-func-mode, so we don't also
    need to do it.

--- modules/modo-completion.el
+++ modules/modo-completion.el
@@ -231,10 +231,7 @@ targets."
                       (evil-ex-search-activate-highlight evil-ex-search-pattern)))))))
   (advice-add 'consult-line :around #'modo--consult-line-evil-ex)
   (setq consult-project-root-function #'projectile-project-root)
-  (defun modo--maybe-update-which-func ()
-    (when which-func-mode
-      (which-func-update)))
-  (add-hook 'consult-after-jump-hook #'modo--maybe-update-which-func)
+  (add-hook 'consult-after-jump-hook #'which-func-update)
   (consult-customize
    consult-recent-file :preview-key nil))
 
