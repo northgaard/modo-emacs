a402a0b8b7b9f096880a5876dca453af69b78f80
Sebastian Bossolini Nørgaard <sebastian@bsnrg.net>
2023-04-19 11:09:22 +0200

    Make flymake-goto command non-repeatable
    I still have no idea by what mechanism they get *declared* repeatable,
    but whatever.

--- modules/modo-flymake.el
+++ modules/modo-flymake.el
@@ -8,7 +8,10 @@
   :defer 5
   :init
   (with-eval-after-load 'minions
-    (push '(flymake-mode . nil) minions-available-modes)))
+    (push '(flymake-mode . nil) minions-available-modes))
+  :config
+  (evil-declare-not-repeat 'flymake-goto-next-error)
+  (evil-declare-not-repeat 'flymake-goto-prev-error))
 
 (provide 'modo-flymake)
 ;;; modo-flymake.el ends here
